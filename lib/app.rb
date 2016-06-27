7dd247f2fcfa3459511e044061d4286acc9061a1
Stephanie Gurung and Tony Fabery <stephanie@>
2016-06-27 11:16:31 -0700

    is true if coordinates are vertically in line with each other

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,11 @@
 class Array
   define_method(:queen_attack?) do |atk|
-    self[1] == atk[1]
+    if self[1] == atk[1]
+      true
+    elsif self[0] == atk[0]
+      true
+    else
+      false
+    end
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -8,4 +8,7 @@ describe('Array#queen_attack?') do
   it('is true if the coordinates are horizontally in line with each other') do
     expect([1,1].queen_attack?([8, 1])).to(eq(true))
   end
+  it('is true if the coordinates are vertically in line with each other') do
+    expect([1,1].queen_attack?([1, 8])).to(eq(true))
+  end
 end
